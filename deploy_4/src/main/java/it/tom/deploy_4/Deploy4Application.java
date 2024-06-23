48d58aefb77b22e08793c6d0c54d0f9a40a1b1f7
FusRoDahh <67054798+FusRoDahh@users.noreply.github.com>
2024-06-23 12:45:30 +0200

    deploy_4

---
+++ deploy_4/src/main/java/it/tom/deploy_4/Deploy4Application.java
@@ -0,0 +1,13 @@
+package it.tom.deploy_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy4Application.class, args);
+	}
+
+}
---
+++ deploy_4/src/test/java/it/tom/deploy_4/Deploy4ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tom.deploy_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
